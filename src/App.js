51a4c3fec2e0fa43ed1c832158dcb450c5b9fe92
perunon <patryk.pilarczyk.00@gmail.com>
2022-05-27 09:40:57 +0200

    Added React Router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
+import { Route, Routes } from "react-router-dom";
+
 function App() {
-  return <div>hi</div>;
+  return (
+    <Routes>
+      <Route path="/" element={<h1>This is home page</h1>}></Route>
+      <Route path="starring" element={<h1>Starring site</h1>}></Route>
+      <Route path="*" element={<h1>CHUJ</h1>}></Route>
+    </Routes>
+  );
 }
 
 export default App;
