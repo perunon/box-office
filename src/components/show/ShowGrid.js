217293daa9b3d750fb98ebd7c3a76d212540332f
perunon <patryk.pilarczyk.00@gmail.com>
2022-06-14 16:14:10 +0200

    Using custom hooks for starred shows

--- src/components/show/ShowGrid.js
+++ src/components/show/ShowGrid.js
@@ -3,19 +3,36 @@ import ShowCard from "./ShowCard";
 import { FlexGrid } from "../styled.js";
 
 import IMAGE_NOT_FOUND from "../../images/not-found.png";
+import { useShows } from "../../misc/custom-hooks";
 
 const ShowGrid = ({ data }) => {
+  const [starredShows, dispatchStarred] = useShows();
+
   return (
     <FlexGrid>
-      {data.map(({ show }) => (
-        <ShowCard
-          id={show.id}
-          key={show.id}
-          name={show.name}
-          image={show.image ? show.image.medium : IMAGE_NOT_FOUND}
-          summary={show.summary}
-        />
-      ))}
+      {data.map(({ show }) => {
+        const isStarred = starredShows.includes(show.id);
+
+        const onStarClick = () => {
+          if (isStarred) {
+            dispatchStarred({ type: "REMOVE", showId: show.id });
+          } else {
+            dispatchStarred({ type: "ADD", showId: show.id });
+          }
+        };
+
+        return (
+          <ShowCard
+            id={show.id}
+            key={show.id}
+            name={show.name}
+            image={show.image ? show.image.medium : IMAGE_NOT_FOUND}
+            summary={show.summary}
+            onStarClick={onStarClick}
+            isStarred={isStarred}
+          />
+        );
+      })}
     </FlexGrid>
   );
 };
